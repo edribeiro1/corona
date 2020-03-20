ca50e994f0365397c3729f5eeea7740f4f3a7d77
CaioDePaula <caio.teodoro@ftdata.com.br>
2020-03-20 15:07:51 -0300

    filter invert em toda tag imagem

---
+++ main.js
@@ -0,0 +1,67 @@
+var map = {};
+var cluster = {}
+$(function () {
+    $(document).ready(function () {
+        initMapa();
+        getLocation();   
+
+        buscarCasos((casos) => {
+            cluster = L.markerClusterGroup({
+                spiderfyOnMaxZoom :  false ,
+                showCoverageOnHover :  false ,
+                zoomToBoundsOnClick :  true,
+                disableClusteringAtZoom: 17,
+            });
+            let markers = [];
+
+            for (const i in casos) {
+                if (casos.hasOwnProperty(i)) {
+                    let caso = casos[i];
+                    markers.push(createMarker(caso));
+                }
+            }
+            // cluster.addLayers(markers);
+            // map.addLayer(cluster);
+        });
+    });
+});
+
+function getLocation() {
+    map.locate({setView: true, maxZoom: 16});
+    map.on('locationfound', (e) => L.marker(e.latlng).addTo(map));
+}
+
+function initMapa() {
+    map = L.map('mapa', {minZoom: 1}).setView([-22.738045, -47.084488], 5);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        }
+    ).addTo(map);
+}
+
+function createMarker(dado) {
+    return L.circleMarker([dado.lat, dado.lng], {
+        color: '#ff4e4e',
+        fillColor: '#ff4e4e',
+        fillOpacity: 0.5,
+        radius: 6 + (6 * (parseInt(dado.qtde_confirmado)/50))
+    }).bindTooltip(`
+        Cidade: ${dado.cidade}<br>
+        Casos confirmados: ${dado.qtde_confirmado}
+    `).addTo(map);
+}
+
+function buscarCasos(cb) {
+    $.ajax({
+        // url: "172.19.254.180/api-corona/casos",
+        url: "http://ec2-54-196-120-46.compute-1.amazonaws.com/api-corona/",
+        type: 'get',
+        dataType: 'json',
+        processData: false,
+        contentType: "application/x-www-form-urlencodedy",
+        success: function(resultado) {
+            if (typeof cb == "function") {
+                cb(resultado)
+            }
+        }
+    });         
+}
