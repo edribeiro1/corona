dc0697865f7b4476007e339f908cb2ad607ba15f
CaioDePaula <caio.teodoro@ftdata.com.br>
2020-03-21 13:24:37 -0300

    feed noticias para desktop

--- main.js
+++ main.js
@@ -4,15 +4,35 @@ var zonaSegura = false;
 
 $(function () {
     $(document).ready(function () {
+        $(document).on('click', '.template-cidade', function (ev) {
+            let self = $(this);
+            map.setView([self.data('lat'),self.data('lng')], 15);            
+        });
+        
         initMapa();
         buscarCasos((casos) => {
             let markers = [];
+            let totalConfirmado = 0;
+            let totalSuspeito = 0;
+            let totalMortos = 0;
             for (const i in casos) {
                 if (casos.hasOwnProperty(i)) {
                     let caso = casos[i];
+                    caso = validarDados(caso);
+
+                    totalConfirmado += caso.qtde_confirmado;
+                    totalSuspeito += caso.qtd_suspeitos;
+                    totalMortos += caso.qtde_mortes;
+
                     markers.push(createMarker(caso));
+                    $('.cities-list').append(templateCities(caso));
                 }
             }
+            $('#totalCasos').text(totalConfirmado + totalSuspeito + totalMortos);
+            $('#totalConfirmado').text(totalConfirmado);
+            $('#totalSuspeito').text(totalSuspeito);
+            $('#totalMortos').text(totalMortos);
+            
             getLocation(casos);
         });
 
@@ -42,21 +62,14 @@ function initMapa() {
     map = L.map('mapa', {minZoom: 1}).setView([-14.2350, -51.9253], 5);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '<a href="https://www.openstreetmap.org/">OpenStreetMap</a>',
-        }
-    ).addTo(map);
+    }).addTo(map);
+    map.zoomControl.setPosition('topright');
 }
 function initAnimationZona() {
     $('div.awesome-marker-shadow.awesome-marker.leaflet-zoom-animated').addClass((zonaSegura)? 'pulseOutZonaSegura': 'pulseOutZonaPerigo');
 }
 
 function createMarker(dado) {
-    dado.qtde_confirmado = $.isNumeric(dado.qtde_confirmado)? parseInt(dado.qtde_confirmado) : 0;
-    dado.qtd_suspeitos = $.isNumeric(dado.qtd_suspeitos)? parseInt(dado.qtd_suspeitos) : 0;
-    dado.qtde_mortes = $.isNumeric(dado.qtde_mortes)? parseInt(dado.qtde_mortes) : 0;
-
-    dado.lat = $.isNumeric(dado.lat)? parseInt(dado.lat) : 0;
-    dado.lng = $.isNumeric(dado.lng)? parseInt(dado.lng) : 0;
-
     return L.circleMarker([dado.lat, dado.lng], {
         color: '#ff4e4e',
         fillColor: '#ff4e4e',
@@ -69,7 +82,7 @@ function templateTooltip(dado) {
     let totalConfirmado = dado.qtde_confirmado;
     let totalSuspeito = dado.qtd_suspeitos;
     let totalMortos = dado.qtde_mortes;
-    let totalCasos = totalConfirmado + totalSuspeito;
+    let totalCasos = totalConfirmado + totalSuspeito + totalMortos;
     
     return `
         <div class="tooltip-info">
@@ -103,6 +116,24 @@ function templateTooltip(dado) {
         </div>
     `;
 }
+function templateCities(dado) {
+    let cidade = (dado.cidade)? dado.cidade : '';
+    let totalConfirmado = dado.qtde_confirmado;
+    let totalSuspeito = dado.qtd_suspeitos;
+    let totalMortos = dado.qtde_mortes;
+    let lat = dado.lat;
+    let lng = dado.lng;
+    let totalCasos = totalConfirmado + totalSuspeito + totalMortos;
+
+    return `
+        <li>
+            <div class="template-cidade" data-lat="${lat}" data-lng="${lng}">
+                <span style="margin: 0px 0px 0px 25px">${cidade}</span>
+                <span class="total-casos" style="margin: 0px 25px 0px 0px; font-weight: bold;">${totalCasos}</span>
+            </div>
+        </li>
+    `;
+}
 
 function buscarCasos(cb) {
     $.ajax({
@@ -117,4 +148,14 @@ function buscarCasos(cb) {
             }
         }
     });         
+}
+
+function validarDados(dado) {
+    dado.qtde_confirmado = $.isNumeric(dado.qtde_confirmado)? parseInt(dado.qtde_confirmado) : 0;
+    dado.qtd_suspeitos = $.isNumeric(dado.qtd_suspeitos)? parseInt(dado.qtd_suspeitos) : 0;
+    dado.qtde_mortes = $.isNumeric(dado.qtde_mortes)? parseInt(dado.qtde_mortes) : 0;
+    dado.lat = $.isNumeric(dado.lat)? dado.lat : 0;
+    dado.lng = $.isNumeric(dado.lng)? dado.lng : 0;
+
+    return dado;
 }
