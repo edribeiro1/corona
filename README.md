f93f52d9331318584aa2090ca2a4733ea3d72e1d
Edson Ribeiro <edsonribeirojr95@gmail.com>
2025-04-09 02:10:49 -0300

    feat: js adjustment and css refactor

---
+++ README.md
@@ -0,0 +1,6 @@
+# Corona Brasil Leaflet
+
+Projeto para testar algumas funcionalidades da ferramenta de Mapa Leaflet.
+
+## Demonstração
+Acesse o [link](https://edribeiro1.github.io/corona/).
--- main.js
+++ main.js
@@ -4,136 +4,50 @@ var zonaSegura = false;
 
 $(function () {
     $(document).ready(function () {
-        $(document).on('click', '.template-cidade', function (ev) {
-            let self = $(this);
-            map.setView([self.data('lat'),self.data('lng')], ($(self).closest('li').hasClass('brasil'))? 4 : 12);
-
-            if ($('.container-cities-list').hasClass("selecionar-cidade")) {
-                $('.selecionar-cidade').removeClass('selecionar-cidade');
-                let totalConfirmado = self.data("confirmado");
-                let totalSuspeito = self.data("suspeito");
-                let totalMortos = self.data("mortos");
-                let cidade = self.data("cidade");
-
-                $('#cidadeSelecionadaNome').text(cidade);
-                $('#cidadeSelecionadaTotalCasos').text((totalConfirmado + totalSuspeito + totalMortos));
-                setTotalCountTo('#totalCasos', (totalConfirmado + totalSuspeito + totalMortos));
-                setTotalCountTo('#totalConfirmado', totalConfirmado);
-                setTotalCountTo('#totalSuspeito', totalSuspeito);
-                setTotalCountTo('#totalMortos', totalMortos);
-                setTotalCountTo('#totalVivos', parseFloat(209), ()=> $('#totalVivos').text(`${$('#totalVivos').text()}.3 milhões`));
-            }
-        });
         $(document).on('click', '.template-estado', function (ev) {
-            let self = $(this).closest('li');
-            if (!$(self).hasClass('selecionado')) {
-                $('.selecionado').removeClass("selecionado");                
-            }
-            $(self).toggleClass("selecionado");
+            // let self = $(this).closest('li');
+            // if (!$(self).hasClass('selecionado')) {
+            //     $('.selecionado').removeClass("selecionado");                
+            // }
+            // $(self).toggleClass("selecionado");
         });
-        $(document).on('click', '.template-cidade-selecionada', function (params) {
-           $('.container-cities-list').addClass("selecionar-cidade");
-        });
-        
         initMapa();
         buscarCasos((casos) => {
             let markers = [];
             let totalConfirmado = 0;
-            let totalSuspeito = 0;
             let totalMortos = 0;
-            let agrupamentoEstado = {};
 
             for (const i in casos) {
                 if (casos.hasOwnProperty(i)) {
                     let caso = casos[i];
-                    caso = validarDados(caso);
-
-                    totalConfirmado += caso.qtde_confirmado;
-                    totalSuspeito += caso.qtde_suspeito;
-                    totalMortos += caso.qtde_mortes;
-
-                    if (Array.isArray(agrupamentoEstado[caso.estado])) {
-                        agrupamentoEstado[caso.estado].push(templateCities(caso))
-                        agrupamentoEstado[caso.estado].totalCasos += (caso.qtde_confirmado + caso.qtde_suspeito + caso.qtde_mortes);
-                    } else {
-                        agrupamentoEstado[caso.estado] = [templateCities(caso)];
-                        agrupamentoEstado[caso.estado].totalCasos = (caso.qtde_confirmado + caso.qtde_suspeito + caso.qtde_mortes);
-                    }
-
+                    totalConfirmado += caso.confirmed;
+                    totalMortos += caso.deaths;
                     markers.push(createMarker(caso));
                 }
             }
 
-            for (const estado in agrupamentoEstado) {
-                if (agrupamentoEstado.hasOwnProperty(estado)) {
-                    let cidades = agrupamentoEstado[estado];
-                    let total = agrupamentoEstado[estado].totalCasos;
-                    $('.cities-list').append(templateStates(estado, total, cidades));
-                }
-            }
-            $('.cities-list').prepend($(templateCities({
-                'cidade':"Brasil",
-                'qtde_confirmado': totalConfirmado,
-                'qtde_suspeito': totalSuspeito,
-                'qtde_mortes': totalMortos,
-                'lat': -14.2350,
-                'lng': -51.9253
-            })).addClass('brasil'));
 
-            $('#cidadeSelecionadaNome').text("Brasil");
-            $('#cidadeSelecionadaTotalCasos').text((totalConfirmado + totalSuspeito + totalMortos));
-
-            setTotalCountTo('#totalCasos', (totalConfirmado + totalSuspeito + totalMortos));
-            setTotalCountTo('#totalConfirmado', totalConfirmado);
-            setTotalCountTo('#totalSuspeito', totalSuspeito);
-            setTotalCountTo('#totalMortos', totalMortos);
-            setTotalCountTo('#totalVivos', parseFloat(209), ()=> $('#totalVivos').text(`${$('#totalVivos').text()}.3 milhões`));
+            const totalSoma = totalConfirmado + totalMortos
+            $('#estadoSelecionadoNome').text("Brasil");
+            $('#estadoSelecionadoTotalCasos').text(formatNumber(totalSoma));
+            $('#totalCasos').text(formatNumber(totalSoma));
+            $('#totalConfirmado').text(formatNumber(totalConfirmado));
+            $('#totalMortos').text(formatNumber(totalMortos));
+            $('#totalVivos').text(formatNumber(212583750));
             
-            getLocation(casos);
-        });
-        buscarNews((result) => {
-            let noticias_buscadas = [];
-            result.noticias.forEach(noticia => {
-                noticias_buscadas.push(convertXmlToJSon(noticia));
-            });
-            if (!noticias_buscadas.length) {
-                return;
-            }
-            noticias_buscadas.forEach(noticia_buscada => {
-                if (noticia_buscada) {
-                    let noticias = {};
-                    noticias = noticia_buscada.rss.channel;
-                    noticias.item.forEach(noticia => {
-                        if (noticia.title.toLocaleLowerCase().search('coronavirus') > -1 || noticia.title.toLocaleLowerCase().search('covid-19') > -1 ) {
-                            $('.news').append(templateNews(noticia));
-                        }
-                    });
-                }
-            });
-            $('.news').removeClass('none');
+            // setTotalCountTo('#estadoSelecionadoTotalCasos', totalSoma);
+            // setTotalCountTo('#totalCasos', totalSoma);
+            // setTotalCountTo('#totalConfirmado', totalConfirmado);
+            // setTotalCountTo('#totalMortos', totalMortos);
+            // setTotalCountTo('#totalVivos', 212583750);
+            
         });
 
         map.on('zoomend', () => {
-            initAnimationZona();
             map.invalidateSize();
         });
     });
 });
-
-function getLocation(casos) {
-    map.locate({setView: true, maxZoom: 5});
-    map.on('locationfound', (e) => {        
-        casos.forEach(caso => {
-            zonaSegura = (parseInt(map.distance(e.latlng, [caso.lat, caso.lng])) > 300);
-        });
-
-        let icon = L.AwesomeMarkers.icon({
-            'markerColor': (zonaSegura)? "green" : "red"
-        });
-        L.marker(e.latlng, {'icon': icon}).addTo(map);
-        initAnimationZona()
-    });
-}
 
 function initMapa() {
     map = L.map('mapa', {minZoom: 1}).setView([-14.2350, -51.9253], 5);
@@ -142,31 +56,29 @@ function initMapa() {
     }).addTo(map);
     map.zoomControl.setPosition('topright');
 }
-function initAnimationZona() {
-    $('div.awesome-marker-shadow.awesome-marker.leaflet-zoom-animated').addClass((zonaSegura)? 'pulseOutZonaSegura': 'pulseOutZonaPerigo');
-}
 
 function createMarker(dado) {
+
+    const somaTotal = dado.confirmed + dado.deaths
     return L.circleMarker([dado.lat, dado.lng], {
         color: '#d60000',
         fillColor: '#d60000',
-        fillOpacity: 1,
-        radius: 4 + (parseInt(dado.qtde_confirmado)/100)
+        fillOpacity: 0.5,
+        radius: 10 + (parseInt(somaTotal)/100000)
     }).bindTooltip(templateTooltip(dado)).addTo(map);
 }
 function templateTooltip(dado) {
-    let cidade = (dado.cidade)? dado.cidade : '';
-    let totalConfirmado = dado.qtde_confirmado;
-    let totalSuspeito = dado.qtde_suspeito;
-    let totalMortos = dado.qtde_mortes;
-    let totalCasos = totalConfirmado + totalSuspeito + totalMortos;
+    let state = (dado.state)? dado.state : '';
+    let totalConfirmado = dado.confirmed;
+    let totalMortos = dado.deaths;
+    let totalCasos = totalConfirmado + totalMortos;
     
     return `
         <div class="tooltip-info">
-            <label>${cidade}</label>
+            <label>${state}</label>
             <div>
                 <div class="total-casos">Total de casos:</div>
-                <span>${totalCasos}</span>
+                <span>${formatNumber(totalCasos)}</span>
             </div>
             <hr>
             <div>
@@ -174,124 +86,69 @@ function templateTooltip(dado) {
                     <div class="casos-notificacao confirmado"></div>
                     Confirmados:
                 </span>
-                <span>${totalConfirmado}</span>
-            </div>
-            <div>
-                <span class="casos-ocorridos">
-                    <div class="casos-notificacao suspeito"></div>
-                    Suspeitas:
-                </span>
-                <span>${totalSuspeito}</span>
+                <span>${formatNumber(totalConfirmado)}</span>
             </div>
             <div>
                 <span class="casos-ocorridos">
                     <div class="casos-notificacao mortos"></div>
                     Mortos:
                 </span>
-                <span>${totalMortos}</span>
+                <span>${formatNumber(totalMortos)}</span>
             </div>
         </div>
     `;
 }
-function templateCities(dado) {
-    let cidade = (dado.cidade)? dado.cidade : '';
-    let totalConfirmado = dado.qtde_confirmado;
-    let totalSuspeito = dado.qtde_suspeito;
-    let totalMortos = dado.qtde_mortes;
-    let lat = dado.lat;
-    let lng = dado.lng;
-    let totalCasos = totalConfirmado + totalSuspeito + totalMortos;
-
-    return `
-        <li>
-            <div class="template-cidade"
-                data-lat="${lat}" 
-                data-lng="${lng}" 
-                data-confirmado="${totalConfirmado}"
-                data-suspeito="${totalSuspeito}"
-                data-mortos="${totalMortos}"
-                data-cidade="${cidade}">
-
-                <span style="margin: 0px 0px 0px 25px">${cidade}</span>
-                <span class="total-casos" style="margin: 0px 25px 0px 0px; font-weight: bold;">${totalCasos}</span>
-            </div>
-        </li>
-    `;
-}
-function templateStates(estado, totalCasos, cidades) {
-    let li = `
-        <li>
-            <div class="template-estado">
-                <span style="margin: 0px 0px 0px 25px; max-width: 245px;">${estado}</span>
-                <span class="total-casos" style="margin: 0px 25px 0px 0px; font-weight: bold;">
-                    ${totalCasos}
-                    <i class="fa fa-chevron-down" style="color: rgba(255, 255, 255, 0.7);"></i>
-                </span>
-            </div>            
-        </li>
-    `;
-
-    li = $(li).append($('<div class="estado-lista-cidades"></div>').append(cidades.map((cidade) => cidade)));
-    return li;
-}
 
 function buscarCasos(cb) {
-    $.ajax({
-        url: "http://ec2-54-196-120-46.compute-1.amazonaws.com/api-corona/",
-        type: 'get',
-        dataType: 'json',
-        processData: false,
-        contentType: "application/x-www-form-urlencodedy",
-        success: function(resultado) {
-            if (typeof cb == "function") {
-                cb(resultado)
-            }
-        }
-    });         
-}
-function buscarNews(cb) {
-    $.ajax({
-        // url: "http://ec2-54-196-120-46.compute-1.amazonaws.com/api-corona/noticias",
-        // url: "http://[::1]/corona/noticias.php/",
-        type: 'get',
-        dataType: 'json',
-        processData: false,
-        contentType: "application/x-www-form-urlencodedy",
-        success: function(resultado) {
-            if (typeof cb == "function") {
-                cb(resultado);
-            }
-        }
-    });
-}
-function convertXmlToJSon(xml) {
-    let x2js = new X2JS();
-    return x2js.xml_str2json(xml);
-}
-function templateNews(news) {
-    let imagens_substitutas = ["coronavirus-img-noticia-default.jpg", "coronavirus-img-noticia-default-2.jpg", "coronavirus-img-noticia-default-3.jpg"]
+    cb([
+        {"lat": -23.5505, "lng": -46.6333, "state": "Sao Paulo, Brazil",  "confirmed": 4119265, "deaths": 140809, "date": "2021-08-10"},
+        {"lat": -18.5122, "lng": -44.555, "state": "Minas Gerais, Brazil", "confirmed": 1998878, "deaths": 51343, "date": "2021-08-10"},
+        {"lat": -25.2521, "lng": -52.0215, "state": "Parana, Brazil", "confirmed": 1403372, "deaths": 35928, "date": "2021-08-10"},
+        {"lat": -30.0346, "lng": -51.2177, "state": "Rio Grande do Sul, Brazil", "confirmed": 1381971, "deaths": 33588, "date": "2021-08-10"},
+        {"lat": -12.5797, "lng": -41.7007, "state": "Bahia, Brazil", "confirmed": 1202820, "deaths": 25983, "date": "2021-08-10"},
+        {"lat": -27.2423, "lng": -50.2189, "state": "Santa Catarina, Brazil", "confirmed": 1124632, "deaths": 18204, "date": "2021-08-10"},
+        {"lat": -22.9068, "lng": -43.1729, "state": "Rio de Janeiro, Brazil", "confirmed": 1061793, "deaths": 59990, "date": "2021-08-10"},
+        {"lat": -5.4984, "lng": -39.3206, "state": "Ceara, Brazil", "confirmed": 923331, "deaths": 23692, "date": "2021-08-10"},
+        {"lat": -15.827, "lng": -49.8362, "state": "Goias, Brazil", "confirmed": 761655, "deaths": 21304, "date": "2021-08-10"},
+        {"lat": -8.8137, "lng": -36.9541, "state": "Pernambuco, Brazil", "confirmed": 595942, "deaths": 19028, "date": "2021-08-10"},
+        {"lat": -1.9981, "lng": -54.9306, "state": "Para, Brazil", "confirmed": 575680, "deaths": 16169, "date": "2021-08-10"},
+        {"lat": -19.1834, "lng": -40.3089, "state": "Espirito Santo, Brazil", "confirmed": 547282, "deaths": 11989, "date": "2021-08-10"},
+        {"lat": -12.6819, "lng": -56.9211, "state": "Mato Grosso, Brazil", "confirmed": 496059, "deaths": 12800, "date": "2021-08-10"},
+        {"lat": -15.7998, "lng": -47.8645, "state": "Distrito Federal, Brazil", "confirmed": 455682, "deaths": 9741, "date": "2021-08-10"},
+        {"lat": -7.24, "lng": -36.782, "state": "Paraiba, Brazil", "confirmed": 425425, "deaths": 9065, "date": "2021-08-10"},
+        {"lat": -3.4168, "lng": -65.8561, "state": "Amazonas, Brazil", "confirmed": 419744, "deaths": 13591, "date": "2021-08-10"},
+        {"lat": -5.4026, "lng": -36.9541, "state": "Rio Grande do Norte, Brazil", "confirmed": 361034, "deaths": 7188, "date": "2021-08-10"},
+        {"lat": -20.7722, "lng": -54.7852, "state": "Mato Grosso do Sul, Brazil", "confirmed": 360499, "deaths": 9082, "date": "2021-08-10"},
+        {"lat": -4.9609, "lng": -45.2744, "state": "Maranhao, Brazil", "confirmed": 340477, "deaths": 9752, "date": "2021-08-10"},
+        {"lat": -7.7183, "lng": -42.7289, "state": "Piaui, Brazil", "confirmed": 311784, "deaths": 6877, "date": "2021-08-10"},
+        {"lat": -10.5741, "lng": -37.3857, "state": "Sergipe, Brazil", "confirmed": 275776, "deaths": 5938, "date": "2021-08-10"},
+        {"lat": -11.5057, "lng": -63.5806, "state": "Rondonia, Brazil", "confirmed": 259832, "deaths": 6407, "date": "2021-08-10"},
+        {"lat": -9.5713, "lng": -36.782, "state": "Alagoas, Brazil", "confirmed": 231783, "deaths": 5905, "date": "2021-08-10"},
+        {"lat": -10.1753, "lng": -48.2982, "state": "Tocantins, Brazil", "confirmed": 212851, "deaths": 3569, "date": "2021-08-10"},
+        {"lat": 0.902, "lng": -52.003, "state": "Amapa, Brazil", "confirmed": 121725, "deaths": 1926, "date": "2021-08-10"},
+        {"lat": -2.7376, "lng": -62.0751, "state": "Roraima, Brazil", "confirmed": 121043, "deaths": 1890, "date": "2021-08-10"},
+        {"lat": -9.0238, "lng": -70.812, "state": "Acre, Brazil", "confirmed": 87422, "deaths": 1804,"date": "2021-08-10"}
+    ])
 
-    let linkImage = (news.description.__cdata)? news.description.__cdata : news.description;
-    linkImage = linkImage.slice(linkImage.search('https:'), linkImage.search('.jpg') + 5);
-    if (!linkImage) {
-        linkImage = imagens_substitutas[Math.floor(Math.random() * (3 - 0))];
-    }
-    let link = news.link;
-    let title = news.title;
-
-    return `
-        <a href="${link}" class="template-noticia">
-            <img class="news-image" src="${linkImage}">
-            <div> ${title} </div>
-        </a>
-    `;
+    // $.ajax({
+    //     url: "http://ec2-54-196-120-46.compute-1.amazonaws.com/api-corona/",
+    //     type: 'get',
+    //     dataType: 'json',
+    //     processData: false,
+    //     contentType: "application/x-www-form-urlencodedy",
+    //     success: function(resultado) {
+    //         if (typeof cb == "function") {
+    //             cb(resultado)
+    //         }
+    //     }
+    // });         
 }
 
 function setTotalCountTo(element, data, cb) {
     $(element).countTo({
         from: 0,
         to: data,
-        speed: 3000,
+        speed: 1000,
         refreshInterval: 50,
         onComplete: function() {
           if (typeof cb == "function") {
@@ -301,12 +158,6 @@ function setTotalCountTo(element, data, cb) {
     });
 }
 
-function validarDados(dado) {
-    dado.qtde_confirmado = $.isNumeric(dado.qtde_confirmado)? parseInt(dado.qtde_confirmado) : 0;
-    dado.qtde_suspeito = $.isNumeric(dado.qtde_suspeito)? parseInt(dado.qtde_suspeito) : 0;
-    dado.qtde_mortes = $.isNumeric(dado.qtde_mortes)? parseInt(dado.qtde_mortes) : 0;
-    dado.lat = $.isNumeric(dado.lat)? dado.lat : 0;
-    dado.lng = $.isNumeric(dado.lng)? dado.lng : 0;
-
-    return dado;
+function formatNumber(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
 }
